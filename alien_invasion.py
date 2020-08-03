0ebc79ed1e1e111a1705b7a76730d35fbe805a72
Aili Gong <aili.hgong@gmail.com>
2020-08-03 15:32:52 -0700

    Moving ship left and right using keys

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,8 @@ class AlienInvasion:
         while True:
             # Event loop to watch for keyboard and mouse events
             self._check_events()
+            # Ship's position is updated after we've checked for keyboard events and before we update the screen.
+            self.ship.update()
             self.__update_screen()
 
 
@@ -42,6 +44,18 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Set move the ship to the right to True.
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    # Set move the ship to the left to True.
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     
     def __update_screen(self):
--- ship.py
+++ ship.py
@@ -23,6 +23,21 @@ class Ship:
         # make ship.rect.midbottom = screen_rect.midbottom
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    
+    def update(self):
+        """
+        Update the ship's position based on the movement flag.
+        """
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
+
     def blitme(self):
         """
         Draw the ship at its current location.
