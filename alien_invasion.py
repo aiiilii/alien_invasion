6f767a72158439ccf04a385d3afe86044a3ccca5
Aili Gong <aili.hgong@gmail.com>
2020-08-03 13:27:58 -0700

    first commit

---
+++ alien_invasion.py
@@ -0,0 +1,40 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """
+    Overall class to manage game assets and behavior.
+    """
+
+    def __init__(self):
+        """
+        Initialize the game, and create game resources
+        """
+        pygame.init()
+
+        # Create display window using a tuple
+        # assign this display window to self.screen, so it is available in all methods in the class
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+
+    def run_game(self):
+        """
+        Start the main loop for the game.
+        """
+        while True:
+            # Event loop to watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Make the most recently drawn screen visible
+            # Continuously updates the display to show the new positions of game elements
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game
+    ai = AlienInvasion()
+    ai.run_game()
