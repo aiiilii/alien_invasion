fdfcaebadad266f0788470f112c85be4b1bc316a
Aili Gong <aili.hgong@gmail.com>
2020-08-04 14:24:06 -0700

    Fleet able to move left and right then dropping down

--- alien_invasion.py
+++ alien_invasion.py
@@ -48,6 +48,8 @@ class AlienInvasion:
             self.ship.update()
             # Update bullets
             self._update_bullets()
+            # Update aliens moving left or right and down
+            self._update_aliens()
             # Update the whole pygame screen
             self.__update_screen()
 
@@ -116,6 +118,15 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
         # print(len(self.bullets))
 
+    
+    def _update_aliens(self):
+        """
+        Check if the fleet is at an edge,
+        then update the positions of all aliens in the fleet.
+        """
+        self._check_fleet_edges()
+        self.aliens.update()
+
 
     def _create_fleet(self):
         """
@@ -147,6 +158,25 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+
+    def _check_fleet_edges(self):
+        """
+        Respond appropriately if any aliens have reached an edge.
+        """
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+
+    def _change_fleet_direction(self):
+        """
+        Drop the entire fleet and chane the fleet's direction.
+        """
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     
     def __update_screen(self):
