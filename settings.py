8804ed3908f1fbbd6c79ac39364d8d3f3c2d5f7e
Aili Gong <aili.hgong@gmail.com>
2020-08-04 14:43:16 -0700

    Repopulated fleet after fleet is destroyed

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ class Settings:
         # Bullet settings
         # bullets travel slightly slower than the ship
         self.bullet_speed = 1.0
-        self.bullet_width = 3
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
